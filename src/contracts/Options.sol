157b86575cb6ce1b4614d2d74ee9311b1486f338
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-09-20 09:36:50 +1000

    🎉 Update

--- src/contracts/Options.sol
+++ src/contracts/Options.sol
@@ -1,36 +1,80 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 contract Options {
     // Have some events to be emmitted as well
 
     // Admin access
     address private owner;
 
-    // Option object
+    // Option data
     struct Option {
         uint256 expiry;
         bool exercised;
         address writer;
         address tokenAddress;
-        uint256 price;
+        uint256 amount;
         string optionType;
     }
 
-    // Store options
     uint256 private optionId;
-    mapping(address => uint256) private options;
+    mapping(uint256 => Option) private options;
+    mapping(address => uint256) private optionOwners;
+
+    // Trade data
+    struct Trade {
+        address poster;
+        uint256 optionId;
+        uint256 price;
+        string status; // Open, closed, cancelled
+    }
+
+    uint256 private tradeId;
+    mapping(uint256 => Trade) private trades;
 
     constructor() {
         // Set the admin as the deployer of the contract
         owner = msg.sender;
     }
 
-    function writeOption(string memory optionType, uint256 amount, uint256 expiry) public {
+    // ============= Option functions =============
+    function writeOption(string memory optionType, uint256 hoursToExpire, address tokenAddress, uint256 amount) public returns (uint256) {
+        // Transfer the tokens from the writers account to the contract
+        IERC20(tokenAddress).transferFrom(msg.sender, address(this), amount); 
 
+        // Write a new option
+        Option memory option = Option({
+            expiry: block.timestamp + hoursToExpire * 1 hours,
+            exercised: false,
+            writer: msg.sender,
+            tokenAddress: tokenAddress,
+            amount: amount,
+            optionType: optionType
+        });
     }
 
-    function getOption(uint256 optionId) public view returns (uint256, bool, address, uint256, string memory) {
+    function getOption(uint256 _optionId) public view returns (uint256, bool, address, uint256, string memory) {
+        // Get the data for an existing option
+        Option memory option = options[_optionId];
+        return (option.expiry, option.exercised, option.tokenAddress, option.amount, option.optionType);
+    }
 
+    function collectExpiredOption(uint256 _optionId) public {
+        // Allow the writer of an option to collect the tokens from their expired option
+    }
+
+    // ============= Marketplace functions =============
+    function executeTrade() public {
+        // Execute a trade for buying an option
+    }
+
+    function openTrade() public {
+        // Open a new trade for selling an option
+    }
+
+    function cancelTrade() public {
+        // Cancel a trade
     }
 }
