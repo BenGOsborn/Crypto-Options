9942a7a94734f4a79f330d93919f6ba347975cf7
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-09-21 09:22:03 +1000

    🎉 Update

---
+++ src/test/optionsMarket.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+const optionsMarket = artifacts.require("OptionsMarket");
+
+// Store DAI address
+const STABLECOIN = "0x6b175474e89094c44da98b954eedeac495271d0f";
+const STABLECOIN_WHALE = "0x47ac0fb4f2d84898e4d9e7b4dab3c24507a6d503";
+
+contract("OptionsMarket", (accounts) => {});
