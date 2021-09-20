fc76d835e22d5408b158ac91024eb97a85c2d6ba
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-09-21 09:42:39 +1000

    🎉 Update

--- src/loadEnv.js
+++ src/loadEnv.js
@@ -1,5 +1,3 @@
-require("dotenv").config();
-
-// Get stablecoin addresses
-exports.STABLECOIN = process.env.STABLECOIN;
-exports.STABLECOIN_WHALE = process.env.STABLECOIN_WHALE;
+// Stablecoin addresses
+exports.STABLECOIN = "0x6b175474e89094c44da98b954eedeac495271d0f";
+exports.STABLECOIN_WHALE = "0x47ac0fb4f2d84898e4d9e7b4dab3c24507a6d503";
--- src/migrations/2_deploy_contracts.js
+++ src/migrations/2_deploy_contracts.js
@@ -1,9 +1,6 @@
 const OptionsMarket = artifacts.require("OptionsMarket");
 const { STABLECOIN } = require("../loadEnv");
 
-// Declare stablecoin
-const STABLECOIN = process.env.STABLECOIN;
-
 module.exports = function (deployer) {
     deployer.deploy(OptionsMarket, STABLECOIN);
 };
