9f281b63b2103e10adef67ca8af7d449f2c173bc
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-09-21 09:52:27 +1000

    🎉 Update

--- src/loadEnv.js
+++ src/loadEnv.js
@@ -1,3 +1,7 @@
-// Stablecoin addresses
+// Stablecoin addresses (DAI)
 exports.STABLECOIN = "0x6b175474e89094c44da98b954eedeac495271d0f";
 exports.STABLECOIN_WHALE = "0x47ac0fb4f2d84898e4d9e7b4dab3c24507a6d503";
+
+// Token addresses (UNI)
+exports.TOKEN = "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984";
+exports.TOKEN_WHALE = "0xe3953d9d317b834592ab58ab2c7a6ad22b54075d";
