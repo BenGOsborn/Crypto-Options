a8c6e04f23b06a57b6b52ce171c15c903af593cb
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-09-21 09:24:56 +1000

    🎉 Update

--- src/test/optionsMarket.js
+++ src/test/optionsMarket.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 const optionsMarket = artifacts.require("OptionsMarket");
 
-// Store DAI address
+// Get stablecoin addresses
 const STABLECOIN = process.env.STABLECOIN;
 const STABLECOIN_WHALE = process.env.STABLECOIN_WHALE;
 
-contract("OptionsMarket", (accounts) => {});
+contract("OptionsMarket", (accounts) => {
+    it("should deploy the contract");
+});
