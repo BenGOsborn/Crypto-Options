e7106ccf69f1448fb8e837939acfae750df5269b
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-09-21 13:35:06 +1000

    🎉 Update

--- src/loadEnv.js
+++
@@ -1,7 +0,0 @@
-// Stablecoin addresses (DAI)
-exports.STABLECOIN = "0x6b175474e89094c44da98b954eedeac495271d0f";
-exports.STABLECOIN_WHALE = "0x9aa65464b4cfbe3dc2bdb3df412aee2b3de86687";
-
-// Token addresses (UNI)
-exports.TOKEN = "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984";
-exports.TOKEN_WHALE = "0xa2e4f309076239ab493dc3bcf51af72176c5f8a3";
--- src/test/optionsMarket.js
+++ src/test/optionsMarket.js
@@ -1,13 +1,13 @@
 const OptionsMarket = artifacts.require("OptionsMarket");
 const IERC20 = artifacts.require("IERC20");
-const {
-    STABLECOIN,
-    STABLECOIN_WHALE,
-    TOKEN,
-    TOKEN_WHALE,
-} = require("../loadEnv");
 
 contract("OptionsMarket", (accounts) => {
+    // Load addresses
+    const STABLECOIN = process.env.STABLECOIN;
+    const STABLECOIN_WHALE = process.env.STABLECOIN;
+    const TOKEN = process.env.STABLECOIN;
+    const TOKEN_WHALE = process.env.STABLECOIN;
+
     it("should get the balance of the stablecoin and token whales and approve the contract to spend all their tokens", async () => {
         // Get the stable coin, token, and optionsmarket
         const stableCoin = await IERC20.at(STABLECOIN);
@@ -22,9 +22,9 @@ contract("OptionsMarket", (accounts) => {
         await stableCoin.approve(optionsMarket.address, stableCoinBal, {
             from: STABLECOIN_WHALE,
         });
-        // await token.approve(optionsMarket.address, tokenBal, {
-        //     from: accounts[0],
-        // });
+        await token.approve(optionsMarket.address, tokenBal, {
+            from: TOKEN_WHALE,
+        });
 
         // Check that the allowance matches what was allocated
         const stableCoinAllowance = await stableCoin.allowance(
@@ -36,15 +36,15 @@ contract("OptionsMarket", (accounts) => {
             stableCoinBal,
             "Did not transfer correct amount of stable coin"
         );
-        // const tokenAllowance = await token.allowance(
-        //     accounts[0],
-        //     optionsMarket.address
-        // );
-        // assert.equal(
-        //     tokenAllowance.toString(),
-        //     tokenBal,
-        //     "Did not transfer correct amount of token"
-        // );
+        const tokenAllowance = await token.allowance(
+            TOKEN_WHALE,
+            optionsMarket.address
+        );
+        assert.equal(
+            tokenAllowance.toString(),
+            tokenBal,
+            "Did not transfer correct amount of token"
+        );
     });
 
     // it("should write a new option to expire tommorow", async () => {
