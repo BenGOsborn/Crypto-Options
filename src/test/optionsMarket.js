03cab3d76e40e70ea8bcf01867683152053fbfbe
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-09-21 18:28:14 +1000

    🎉 Update

--- src/test/optionsMarket.js
+++ src/test/optionsMarket.js
@@ -6,15 +6,45 @@ contract("OptionsMarket", (accounts) => {
     require("dotenv").config();
     const STABLECOIN = process.env.STABLECOIN;
     const STABLECOIN_WHALE = process.env.STABLECOIN_WHALE;
+    const TOKEN = process.env.TOKEN;
+    const TOKEN_WHALE = process.env.TOKEN_WHALE;
 
     it("should get the balance of the stablecoin and token whales and approve the contract to spend all their tokens", async () => {
-        console.log(STABLECOIN, STABLECOIN_WHALE);
+        // Get the contract and tokens
+        const optionsMarket = await OptionsMarket.deployed();
+        const stableCoin = await IERC20.at(STABLECOIN);
+        const token = await IERC20.at(TOKEN);
 
-        const coin = await IERC20.at(STABLECOIN);
-        const bal = await coin.balanceOf(STABLECOIN_WHALE);
+        // Get the balance of the whales
+        const stableCoinBal = await stableCoin.balanceOf(STABLECOIN_WHALE);
+        const tokenBal = await token.balanceOf(TOKEN_WHALE);
 
-        await coin.transfer(accounts[0], bal, {
+        // Approve the contract to use tokens
+        await stableCoin.approve(optionsMarket.address, stableCoinBal, {
             from: STABLECOIN_WHALE,
         });
+        await token.approve(optionsMarket.address, tokenBal, {
+            from: TOKEN_WHALE,
+        });
+
+        // Check that the approval was successful
+        const stableCoinAllowance = await stableCoin.allowance(
+            STABLECOIN_WHALE,
+            optionsMarket.address
+        );
+        assert.equal(
+            stableCoinAllowance.toString(),
+            stableCoinBal,
+            "Failed to transfer correct amount of stablecoins"
+        );
+        const tokenAllowance = await token.allowance(
+            TOKEN_WHALE,
+            optionsMarket.address
+        );
+        assert.equal(
+            tokenAllowance.toString(),
+            tokenBal,
+            "Failed to transfer correct amount of tokens"
+        );
     });
 });
