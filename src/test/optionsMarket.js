2b2341e8393517a570d1e5b0fef1a75a2ed4d924
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-09-21 10:17:02 +1000

    🎉 Update

--- src/test/optionsMarket.js
+++ src/test/optionsMarket.js
@@ -1,9 +1,42 @@
 const OptionsMarket = artifacts.require("OptionsMarket");
+const IERC20 = artifacts.require("IERC20");
+const {
+    STABLECOIN,
+    STABLECOIN_WHALE,
+    TOKEN,
+    TOKEN_WHALE,
+} = require("../loadEnv");
 
 contract("OptionsMarket", (accounts) => {
     let optionsMarket;
+    let stableCoin;
+    let token;
 
-    it("should deploy the contract", async () => {
+    it("should verify the contract deployed", async () => {
         optionsMarket = await OptionsMarket.deployed();
+        stableCoin = await IERC20.at(STABLECOIN);
+        token = await IERC20.at(TOKEN);
+    });
+
+    it("should write a new option to expire tommorow", async () => {
+        // Get the expiry date
+        const today = new Date();
+        const expiry = new Date(
+            today.getFullYear(),
+            today.getMonth(),
+            today.getDate() + 1
+        ).getTime();
+
+        // Allow the contract to work
+
+        // Write the contract
+        const optionId = await optionsMarket.writeOption.call(
+            "call",
+            expiry,
+            TOKEN,
+            3,
+            10
+        );
+        console.log(optionId);
     });
 });
