450c48698eefdb19f5194df3bd8c3b0eb116ce13
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-09-21 10:31:38 +1000

    🎉 Update

--- src/test/optionsMarket.js
+++ src/test/optionsMarket.js
@@ -8,35 +8,51 @@ const {
 } = require("../loadEnv");
 
 contract("OptionsMarket", (accounts) => {
-    let optionsMarket;
-    let stableCoin;
-    let token;
+    it("should get the balance of the stablecoin and token whales and approve the contract to spend all their tokens", async () => {
+        // Get the stable coin, token, and optionsmarket
+        const stableCoin = await IERC20.at(STABLECOIN);
+        const token = await IERC20.at(TOKEN);
+        const optionsMarket = await OptionsMarket.deployed();
 
-    it("should verify the contract deployed", async () => {
-        optionsMarket = await OptionsMarket.deployed();
-        stableCoin = await IERC20.at(STABLECOIN);
-        token = await IERC20.at(TOKEN);
-    });
-
-    it("should write a new option to expire tommorow", async () => {
-        // Get the expiry date
-        const today = new Date();
-        const expiry = new Date(
-            today.getFullYear(),
-            today.getMonth(),
-            today.getDate() + 1
-        ).getTime();
-
-        // Allow the contract to work
-
-        // Write the contract
-        const optionId = await optionsMarket.writeOption.call(
-            "call",
-            expiry,
-            TOKEN,
-            3,
-            10
+        // Get the balance of the whales
+        const stableCoinBal = await stableCoin.balanceOf(STABLECOIN_WHALE);
+        const tokenBal = await token.balanceOf(TOKEN_WHALE);
+        console.log(
+            `Stable coin whale balance: ${stableCoinBal}\nToken whale balance: ${tokenBal}`
         );
-        console.log(optionId);
+
+        // Approve the contract to spend the whales tokens
+        const stableCoinSuccess = await stableCoin.approve(
+            optionsMarket.address,
+            stableCoinBal,
+            { from: STABLECOIN_WHALE }
+        );
+        const tokenSuccess = await token.approve(
+            optionsMarket.address,
+            tokenBal,
+            { from: TOKEN_WHALE }
+        );
     });
+
+    // it("should write a new option to expire tommorow", async () => {
+    //     // Get the expiry date
+    //     const today = new Date();
+    //     const expiry = new Date(
+    //         today.getFullYear(),
+    //         today.getMonth(),
+    //         today.getDate() + 1
+    //     ).getTime();
+
+    //     // Allow the contract to work
+
+    //     // Write the contract
+    //     const optionId = await optionsMarket.writeOption.call(
+    //         "call",
+    //         expiry,
+    //         TOKEN,
+    //         3,
+    //         10
+    //     );
+    //     console.log(optionId);
+    // });
 });
