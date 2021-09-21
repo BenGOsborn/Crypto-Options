266613c6f2b8040f97ff8e50bff0569c437fd64d
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-09-21 11:45:11 +1000

    🎉 Update

--- src/test/optionsMarket.js
+++ src/test/optionsMarket.js
@@ -12,36 +12,56 @@ contract("OptionsMarket", (accounts) => {
         // Get the stable coin, token, and optionsmarket
         const stableCoin = await IERC20.at(STABLECOIN);
         const token = await IERC20.at(TOKEN);
-        const optionsMarket = await OptionsMarket.deployed();
+        const optionsMarket = await OptionsMarket.deployed.call();
 
         // Get the balance of the whales
-        const stableCoinBal = await stableCoin.balanceOf(STABLECOIN_WHALE);
-        const tokenBal = await token.balanceOf(TOKEN_WHALE);
+        const stableCoinBal = await stableCoin.balanceOf.call(STABLECOIN_WHALE);
+        const tokenBal = await token.balanceOf.call(TOKEN_WHALE);
         console.log(
             `Stable coin whale balance: ${stableCoinBal}\nToken whale balance: ${tokenBal}`
         );
 
         // Approve the contract to spend the whales tokens
-        const stableCoinSuccess = await stableCoin.approve(
+        const stableCoinSuccess = await stableCoin.approve.call(
             optionsMarket.address,
             stableCoinBal,
             { from: STABLECOIN_WHALE }
         );
-        // assert.equal(
-        //     stableCoinSuccess,
-        //     true,
-        //     "Could not approve contract for stablecoin"
-        // );
-        // const tokenSuccess = await token.approve(
-        //     optionsMarket.address,
-        //     tokenBal,
-        //     { from: TOKEN_WHALE }
-        // );
-        // assert.equal(
-        //     tokenSuccess,
-        //     true,
-        //     "Could not approve contract for token"
-        // );
+        assert.equal(
+            stableCoinSuccess,
+            true,
+            "Could not approve contract for stablecoin"
+        );
+        const tokenSuccess = await token.approve.call(
+            optionsMarket.address,
+            tokenBal,
+            { from: TOKEN_WHALE }
+        );
+        assert.equal(
+            tokenSuccess,
+            true,
+            "Could not approve contract for token"
+        );
+
+        // Check that the allowance matches what was allocated
+        const stableCoinAllowance = await stableCoin.allowance.call(
+            STABLECOIN_WHALE,
+            optionsMarket.address
+        );
+        assert.equal(
+            stableCoinAllowance,
+            stableCoinBal,
+            "Did not transfer correct amount of stable coin"
+        );
+        const tokenAllowance = await token.allowance.call(
+            TOKEN_WHALE,
+            optionsMarket.address
+        );
+        assert.equal(
+            tokenAllowance,
+            tokenBal,
+            "Did not transfer correct amount of token"
+        );
     });
 
     // it("should write a new option to expire tommorow", async () => {
