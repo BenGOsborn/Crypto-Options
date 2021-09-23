2a97ebcaa607daf166581010e3a7144cb97b109f
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-09-23 15:36:36 +1000

    🎉 Update

--- src/frontend/src/index.tsx
+++ src/frontend/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Web3ReactProvider } from "@web3-react/core";
 import Web3 from "web3";
+import "./index.css";
 
 function getLibrary(provider: any) {
     return new Web3(provider);
