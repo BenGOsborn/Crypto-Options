6173cd81420e32ed43e8fa9548fd6fe661f6c907
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-09-23 13:47:49 +1000

    🎉 Update

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,17 +1,25 @@
 import { useWeb3React } from "@web3-react/core";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Web3 from "web3";
 import { injected } from "./components/wallet/connectors";
 import OptionsMarket from "../../dapp/build/contracts/OptionsMarket.json";
 
+interface Web3Data {}
+
 function App() {
     const { active, account, activate, deactivate } = useWeb3React();
-    const library: Web3 = useWeb3React().library;
+    const web3: Web3 = useWeb3React().library;
+    const [web3Data, setWeb3Data] = useState<Web3Data | null>(null);
 
     async function connect() {
         try {
+            // Connect to wallet and store state
             await activate(injected);
             localStorage.setItem("connected", "true");
+
+            // Get the web3 data and store it in the state
+            const networkId = await web3.eth.net.getId();
+            const deployedNetwork = OptionsMarket.networks[networkId];
         } catch (ex) {
             console.log(ex);
         }
