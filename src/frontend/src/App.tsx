0841452c1c426e9bb1f365f3d84fd9645f2bf007
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-09-23 10:27:55 +1000

    🎉 Update

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { useWeb3React } from "@web3-react/core";
-import { injected } from "../components/wallet/connectors";
+import { injected } from "./components/wallet/connectors";
 
 function App() {
     const { active, account, library, connector, activate, deactivate } =
