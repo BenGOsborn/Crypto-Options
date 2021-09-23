977be93dbe3e475238bd695b35be5f3d7d22c243
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-09-23 10:21:01 +1000

    🎉 Update

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { useWeb3React } from "@web3-react/core";
+import { injected } from "../components/wallet/connectors";
 
 function App() {
-    return <div className="App"></div>;
+    const { active, account, library, connector, activate, deactivate } =
+        useWeb3React();
+
+    function connect() {}
+
+    return (
+        <div className="App">
+            <button onClick={connect}>Connect wallet</button>
+            <span>Not connected</span>
+        </div>
+    );
 }
 
 export default App;
