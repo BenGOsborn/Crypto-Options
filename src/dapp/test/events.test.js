b5803951d6dd8caf77e139fcfcdb55cdac6c9a67
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-09-24 17:52:31 +1000

    🎉 Update

---
+++ src/dapp/test/events.test.js
@@ -0,0 +1,22 @@
+// Uninstall web3
+require("dotenv").config();
+const Web3 = require("web3");
+const OptionsMarket = require("../build/contracts/OptionsMarket.json");
+
+const init = async () => {
+    const web3 = new Web3(`https://ropsten.infura.io/v3/${process.env.INFURA}`);
+
+    const id = await web3.eth.net.getId();
+    const deployedNetwork = OptionsMarket.networks[id];
+    const contract = new web3.eth.Contract(
+        OptionsMarket.abi,
+        deployedNetwork.address
+    );
+
+    const results = await contract.getPastEvents("OptionWritten", {
+        fromBlock: 0,
+    });
+    console.log(results);
+};
+
+init();
