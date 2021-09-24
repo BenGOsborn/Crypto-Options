0b59f1277f196e19ee45940e1ba37bd74c5f4980
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-09-24 21:43:52 +1000

    🎉 Update

--- src/dapp/test/optionsMarket.test.js
+++ src/dapp/test/optionsMarket.test.js
@@ -1,6 +1,7 @@
 const OptionsMarket = artifacts.require("OptionsMarket");
 const IERC20 = artifacts.require("IERC20");
-const { BN } = require("@openzeppelin/test-helpers");
+const Web3 = require("web3");
+const BN = Web3.utils.BN;
 
 contract("OptionsMarket", (accounts) => {
     // Load addresses
