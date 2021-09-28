7b7351c4d05b88151111ac47f95d596d36e97456
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-09-28 16:20:53 +1000

    🎉 Update

--- src/dapp/test/optionsMarket.test.js
+++ src/dapp/test/optionsMarket.test.js
@@ -3,4 +3,24 @@ const IERC20 = artifacts.require("IERC20");
 const Web3 = require("web3");
 const BN = Web3.utils.BN;
 
-contract("OptionsMarket", (accounts) => {});
+contract("OptionsMarket", (accounts) => {
+    // Load addresses
+    require("dotenv").config();
+    const STABLECOIN = process.env.STABLECOIN;
+    const STABLECOIN_WHALE = process.env.STABLECOIN_WHALE;
+    const TOKEN = process.env.TOKEN;
+    const TOKEN_WHALE = process.env.TOKEN_WHALE;
+
+    it("should get the trade currency of the contract", async () => {
+        // Get the contract
+        const optionsMarket = await OptionsMarket.deployed();
+
+        // Get the trade currency and compare it to the stable coin
+        const tradeCurrency = await optionsMarket.getTradeCurrency();
+        assert.equal(
+            STABLECOIN.toLowerCase(),
+            tradeCurrency.toLowerCase().toString(),
+            "Trade currency does not natch stablecoin"
+        );
+    });
+});
