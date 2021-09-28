4ff3508ac0af7da2bfbb5194a54ed059f40494b0
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-09-28 16:19:44 +1000

    🎉 Update

---
+++ src/dapp/test/optionsMarket.test.js
@@ -0,0 +1,6 @@
+const OptionsMarket = artifacts.require("OptionsMarket");
+const IERC20 = artifacts.require("IERC20");
+const Web3 = require("web3");
+const BN = Web3.utils.BN;
+
+contract("OptionsMarket", (accounts) => {});
